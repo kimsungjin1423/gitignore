387a625aabbe91578675dadf14e69e0c127aec14
teacher <tjdwlsdkgkwk1@gmail.com>
2023-07-07 11:02:34 +0900

    1.practice4 관련 소스 (1) filter-interccptor-aop학습 (2) test코드 기본 작성법 학습

---
+++ src/main/java/com/tj/edu/practice4/interceptor_filter/controller/AuthApiController.java
@@ -0,0 +1,17 @@
+package com.tj.edu.practice4.interceptor_filter.controller;
+
+
+import com.tj.edu.practice4.interceptor_filter.annotations.AuthUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authapi")
+@AuthUser
+public class AuthApiController {
+    @GetMapping("/get")
+    public String get(){
+        return "authapi/get호출";
+    }
+}
--- src/main/java/com/tj/edu/practice4/interceptor_filter/controller/RestApiController.java
+++ src/main/java/com/tj/edu/practice4/interceptor_filter/controller/RestApiController.java
@@ -1,14 +1,11 @@
-package com.tj.edu.practice4.aop.controller;
+package com.tj.edu.practice4.interceptor_filter.controller;
 
-
-import com.tj.edu.practice4.aop.dto.User;
+import com.tj.edu.practice4.interceptor_filter.dto.User;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("/api")
 public class RestApiController {
-    //    @TimeChecker
     @GetMapping("/user/{id}")
     public String user(@PathVariable String id, @RequestParam(required = false, defaultValue = "김복순") String name) {
         System.out.println("id: " + id);
@@ -17,14 +14,15 @@ public class RestApiController {
     }
 
     @PostMapping("/user")
-    public User user(User user) {
+    public User registUser(User user) {
         System.out.println("id: " + user.getId());
         System.out.println("name: " + user.getName());
 
         return user;
     }
+
     @PutMapping("/user")
-    public  User updateUser (User user){
+    public User updateUser(User user) {
         return user;
     }
 }
